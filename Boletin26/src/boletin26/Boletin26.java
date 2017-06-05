fee5efffc875c2c33fb2a655e0e50c5e09c23107
AlvaroLopez <alvarocobo14@gmail.com>
2017-06-05 19:28:59 +0200

    Initial Commit

---
+++ Boletin26/src/boletin26/Boletin26.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin26;
+
+/**
+ *
+ * @author alopezcobo
+ */
+public class Boletin26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bonoloto obxBonoloto = new Bonoloto();
+        obxBonoloto.setVisible(true);
+        Adivinar obxAdivinar = new Adivinar();
+        obxBonoloto.Adivinar();
+    }
+    
+}
